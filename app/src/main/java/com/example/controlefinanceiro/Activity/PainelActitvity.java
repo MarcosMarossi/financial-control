a65ad83661cb7b8b6757e0e23c39bb26a1cc552f
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2019-11-22 14:06:53 -0300

    Show account balance in PainelActivity

--- app/src/main/java/com/example/controlefinanceiro/Activity/PainelActitvity.java
+++ app/src/main/java/com/example/controlefinanceiro/Activity/PainelActitvity.java
@@ -11,21 +11,32 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.controlefinanceiro.Configurações.ConfigFirebase;
+import com.example.controlefinanceiro.Helper.Base64Custom;
+import com.example.controlefinanceiro.Model.Usuario;
 import com.example.controlefinanceiro.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
+import java.text.DecimalFormat;
+
 public class PainelActitvity extends AppCompatActivity {
 
-    private FirebaseAuth autenticacao = ConfigFirebase.getFireBaseAutenticacao();
     private MaterialCalendarView calendario;
-    private DatabaseReference reference = ConfigFirebase.getDataBaseFirebase();
+    private Double despesaTotal = 0.0, receitaTotal = 0.0, resultado;
+    private TextView textUsuario, textValor;
 
+
+    private DatabaseReference referencia = ConfigFirebase.getDataBaseFirebase();
+    private FirebaseAuth autenticacao =  ConfigFirebase.getFireBaseAutenticacao();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,18 +51,47 @@ public class PainelActitvity extends AppCompatActivity {
                 .setMaximumDate(CalendarDay.from(2019,1,1))
                 .setMaximumDate( CalendarDay.from(2020,6,1))
                 .commit();
-
-
-
         calendario.setOnMonthChangedListener(new OnMonthChangedListener() {
             @Override
             public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
                 Log.i( "data: ", "valor: " + (date.getMonth() + 1) + "/" + date.getYear() );
             }
         });
+
+        textUsuario = findViewById( R.id.textUsuario );
+        textValor = findViewById( R.id.textValor );
+        recuperarResumo();
     }
 
     public void recuperarResumo(){
+
+        String email = autenticacao.getCurrentUser().getEmail();
+        String id = Base64Custom.codificarBase64( email );
+        DatabaseReference usuarioReferencia = referencia.child( "usuarios" ).child( id );
+
+        usuarioReferencia.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Usuario usuario = dataSnapshot.getValue( Usuario.class );
+
+                despesaTotal = usuario.getDespesaTotal();
+                receitaTotal = usuario.getReceitaTotal();
+
+                resultado = receitaTotal - despesaTotal;
+
+                DecimalFormat decimalFormat = new DecimalFormat( "0.##" );
+                String format = decimalFormat.format( resultado );
+
+                textUsuario.setText( "Olá, " + usuario.getNome()+".");
+                textValor.setText("R$ "+format);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        } );
 
     }
 
