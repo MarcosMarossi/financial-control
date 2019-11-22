3ff5a0b94e71fe995768e85db948741d446b0af9
Marcos Marossi <marcos.marossi@fatec.sp.gov.br>
2019-11-22 11:57:07 -0300

    Add validation

--- app/src/main/java/com/example/controlefinanceiro/Activity/DespesasActivity.java
+++ app/src/main/java/com/example/controlefinanceiro/Activity/DespesasActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.controlefinanceiro.Helper.DateCustom;
 import com.example.controlefinanceiro.Model.Movimentacao;
@@ -31,15 +32,52 @@ public class DespesasActivity extends AppCompatActivity {
     }
 
     public void addDespesa(View view){
-        movimentacao = new Movimentacao();
 
-        String data = etData.getText().toString();
+        if(validar()){
+            movimentacao = new Movimentacao();
+            String data = etData.getText().toString();
 
-        movimentacao.setValor( Double.parseDouble( etValor.getText().toString() ) );
-        movimentacao.setData( data );
-        movimentacao.setCategoria( etTitulo.getText().toString() );
-        movimentacao.setDescricao( etDesc.getText().toString() );
-        movimentacao.setTipo( "r" );
-        movimentacao.salvar(data);
+            movimentacao.setValor( Double.parseDouble( etValor.getText().toString() ) );
+            movimentacao.setData( data );
+            movimentacao.setCategoria( etTitulo.getText().toString() );
+            movimentacao.setDescricao( etDesc.getText().toString() );
+            movimentacao.setTipo( "r" );
+            movimentacao.salvar(data);
+        } else {
+            Toast.makeText( this, "Movimentação não criada!", Toast.LENGTH_SHORT ).show();
+        }
+    }
+
+    public Boolean validar(){
+
+        String sValor = etValor.getText().toString();
+        String sData = etData.getText().toString();
+        String sCategoria = etTitulo.getText().toString();
+        String sDescricao = etDesc.getText().toString();
+
+        if(!sValor.isEmpty()){
+            if(!sData.isEmpty()){
+                if(!sCategoria.isEmpty()){
+                    if(!sDescricao.isEmpty()){
+
+                        } else {
+                            Toast.makeText( this, "Você não descreveu a despesa!", Toast.LENGTH_SHORT ).show();
+                            return false;
+                        }
+                    } else {
+                        Toast.makeText( this, "Você não categorizou a despesa!", Toast.LENGTH_SHORT ).show();
+                        return false;
+                    }
+
+                } else {
+                    Toast.makeText( this, "Você não colocou nenhuma data na despesa.", Toast.LENGTH_SHORT ).show();
+                    return false;
+            }
+        } else {
+            Toast.makeText( this, "Você não colocou nenhum valor.", Toast.LENGTH_SHORT ).show();
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/com/example/controlefinanceiro/Activity/PainelActitvity.java
+++ app/src/main/java/com/example/controlefinanceiro/Activity/PainelActitvity.java
@@ -3,31 +3,74 @@ package com.example.controlefinanceiro.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import com.example.controlefinanceiro.Configurações.ConfigFirebase;
 import com.example.controlefinanceiro.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 public class PainelActitvity extends AppCompatActivity {
+
+    private FirebaseAuth autenticacao = ConfigFirebase.getFireBaseAutenticacao();
+    private MaterialCalendarView calendario;
+    private DatabaseReference reference = ConfigFirebase.getDataBaseFirebase();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.act_painel );
         Toolbar toolbar = findViewById( R.id.toolbar );
+        toolbar.setTitle( "" );
         setSupportActionBar( toolbar );
-        getSupportActionBar().hide();
-/*
-        FloatingActionButton fab = findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener() {
+
+        calendario = findViewById(R.id.calendarView);
+        calendario.state().edit()
+                .setMaximumDate(CalendarDay.from(2019,1,1))
+                .setMaximumDate( CalendarDay.from(2020,6,1))
+                .commit();
+
+
+
+        calendario.setOnMonthChangedListener(new OnMonthChangedListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
+            public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
+                Log.i( "data: ", "valor: " + (date.getMonth() + 1) + "/" + date.getYear() );
             }
-        } );*/
+        });
+    }
+
+    public void recuperarResumo(){
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.menu_toolbar, menu );
+        return super.onCreateOptionsMenu( menu );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.MenuSair:
+                autenticacao.signOut();
+                startActivity( new Intent( this, MainActivity.class ) );
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected( item );
     }
 
     public void criarDespesa(View view){
